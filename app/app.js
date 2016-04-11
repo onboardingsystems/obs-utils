5d668d298e850dc459061aca0b51af173965b5ce
brettv <brettv@onboardingsystems.com>
2016-04-11 17:08:28 -0600

    test

--- app/app.js
+++ app/app.js
@@ -1,17 +1,20 @@
 // Forms
-import FormBuilder     from './forms/form-builder'
-import AddressUS       from './forms/address-us'
-import CompoundLayout  from './forms/compound-layout'
-import Error           from './forms/error'
-import Form            from './forms/form'
-import FormattedText   from './forms/formatted-text'
-import Hint            from './forms/hint'
-import Label           from './forms/label'
-import RequiredMarker  from './forms/required-marker'
-import Text            from './forms/text'
-import Textarea        from './forms/textarea'
-export { FormBuilder, AddressUS, CompoundLayout, Error, Form, FormattedText, Hint, Label, RequiredMarker, Text, Textarea }
+const FormBuilder         = require('./forms/form-builder')
+// import FormBuilder     from './forms/form-builder'
+// import AddressUS       from './forms/address-us'
+// import CompoundLayout  from './forms/compound-layout'
+// import Error           from './forms/error'
+// import Form            from './forms/form'
+// import FormattedText   from './forms/formatted-text'
+// import Hint            from './forms/hint'
+// import Label           from './forms/label'
+// import RequiredMarker  from './forms/required-marker'
+// import Text            from './forms/text'
+// import Textarea        from './forms/textarea'
+// export { FormBuilder, AddressUS, CompoundLayout, Error, Form, FormattedText, Hint, Label, RequiredMarker, Text, Textarea }
+//
+// // Formatters
+// import Formatters from './formatters/formatters'
+// export { Formatters }
 
-// Formatters
-import Formatters from './formatters/formatters'
-export { Formatters }
+module.exports = FormBuilder
