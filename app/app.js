55ef9a06e59ab571d447d1bfc10366da995590ca
brettv <brettv@onboardingsystems.com>
2016-04-12 12:13:40 -0600

    stuff

--- app/app.js
+++ app/app.js
@@ -25,16 +25,17 @@
 module.exports = {
   success: true,
   Forms: {
-    // AddressUS:       require('./forms/address-us'),
-    // CompoundLayout:  require('./forms/compound-layout'),
-    Error:           require('./forms/error')//,
-    // Form:            require('./forms/form'),
-    // FormattedText:   require('./forms/formatted-text'),
-    // Hint:            require('./forms/hint'),
-    // Label:           require('./forms/label'),
-    // RequiredMarker:  require('./forms/required-marker'),
-    // Text:            require('./forms/text'),
-    // Textarea:        require('./forms/textarea')
-  }//,
-  // Formatters: require("./formatters/formatters")
+    FormBuilder:     require('./form/form-builder'),
+    AddressUS:       require('./forms/address-us'),
+    CompoundLayout:  require('./forms/compound-layout'),
+    Error:           require('./forms/error'),
+    Form:            require('./forms/form'),
+    FormattedText:   require('./forms/formatted-text'),
+    Hint:            require('./forms/hint'),
+    Label:           require('./forms/label'),
+    RequiredMarker:  require('./forms/required-marker'),
+    Text:            require('./forms/text'),
+    Textarea:        require('./forms/textarea')
+  },
+  Formatters: require("./formatters/formatters")
 }
