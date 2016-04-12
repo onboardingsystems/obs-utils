c6a216196a489f181fdff700d098b7b7f1b4df7e
brettv <brettv@onboardingsystems.com>
2016-04-12 12:48:47 -0600

    test

--- brunch-config.js
+++ brunch-config.js
@@ -1,24 +1,14 @@
 module.exports = {
   files: {
     javascripts: {
-      joinTo: {
-        'app.js': /^app\/app/,
-        'forms/address-us.js': /^app\/forms\/address-us/,
-        'forms/checkbox.js': /^app\/forms\/checkbox/,
-        'forms/compound-layout.js': /^app\/forms\/compound-layout/,
-        'forms/error.js': /^app\/forms\/error/,
-        'forms/form-builder.js': /^app\/forms\/form-builder/,
-        'forms/form.js': /^app\/forms\/form/,
-        'forms/formatted-text.js': /^app\/forms\/formatted-text/,
-        'forms/hint.js': /^app\/forms\/hint/,
-        'forms/label.js': /^app\/forms\/label/,
-        'forms/required-marker.js': /^app\/forms\/required-marker/,
-        'forms/text.js': /^app\/forms\/text/,
-        'forms/textarea.js': /^app\/forms\/textarea/,
-
-        'formatters/formatters.js': /^app\/formatters/,
-
-        'vendor.js': /(?!app)/
+      entryPoints: {
+        'app/app.js': 'app.js'
+      },
+      order: {
+        before: [
+          "react",
+          "react-dom"
+        ]
       }
     }
   },
@@ -32,8 +22,8 @@ module.exports = {
   },
 
   modules: {
-    wrapper: false,
-    definition: false
+    // wrapper: false,
+    // definition: false
   }
 
 };
