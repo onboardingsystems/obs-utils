d8709c95bb344becdb88aea7aa9ebba02a61fc99
brettv <brettv@onboardingsystems.com>
2016-04-20 14:44:07 -0600

    updating brunch

--- brunch-config.js
+++ brunch-config.js
@@ -1,26 +1,15 @@
 module.exports = {
   files: {
     javascripts: {
-      // joinTo: {
-      //   'obs_utils.js': /^app/,
-      //   'vendor.js': /(?!app)/
-      // },
-      entryPoints: {
-        'app/obs_utils.js': 'obs-utils.js'
-      }
+      joinTo: {
+        'obs_utils.js': /^app/,
+        'vendor.js': /(?!app)/
+      },
     }
   },
 
   plugins: {
     babel: {presets: ['es2015', 'react']}
-  },
-
-  modules: {
-    // wrapper: false,
-    // definition: false
-    autoRequire: {
-      'obs-utils.js': ['obs_utils']
-    }
   }
 
 };
